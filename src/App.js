34480250e5507f121db31d404d12a10b946d7ecc
Samson Lasode <samflicks@gmail.com>
2021-04-01 19:57:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDOM from "react-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from "./components/Container"
+import Header from "./components/Header"
+import WeatherSearch from "./components/WeatherSearch"
+import WeatherData from "./components/WeatherData"
+import ErrorMessage from "./components/ErrorMessage"
+
+class App extends React.Component {
+    state = {
+        weather: null,
+        showError: false
+    }
+    api_call = async (e) => {
+        e.preventDefault()
+        const location = e.target.elements.location.value
+        if (location) {
+            const API_KEY = "9c3cb98520f309bd159e77512e8e5e28"
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+            const request = await fetch(url)
+            const response = await request.json()
+            this.setState({weather: response, showError: false})
+        } else {
+            return this.setState({ showError: true, weather: null }) 
+        }
+    }
+    render() {
+        return (
+            <Container>
+                <Header />
+                <WeatherSearch api_call={this.api_call} />
+                { this.state.weather && <WeatherData weatherData={this.state.weather} /> }
+                { this.state.showError && <ErrorMessage /> }
+            </Container>
+        )
+    }
 }
+
+ReactDOM.render(<App/>, document.getElementById("root"))
 
 export default App;
